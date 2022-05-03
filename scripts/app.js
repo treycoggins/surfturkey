3249327e2ea613243265ecf88b36fddcad4ca51b
Trey Coggins <trey@localhost.localdomain>
2022-05-03 00:38:57 -0400

    first commit

---
+++ scripts/app.js
@@ -0,0 +1,18 @@
+// Show/hide contact modal
+const contactBtn = document.getElementById('contactBtn');
+const contactForm = document.getElementById('contactForm');
+const span = document.getElementById('closeForm')
+
+contactBtn.addEventListener('click', () => {
+  contactForm.style.display = 'flex';
+});
+
+span.onclick = function() {
+  contactForm.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == contactForm) {
+    contactForm.style.display = "none";
+  }
+}
