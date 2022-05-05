17353ea551139f851f38e726900154fe75940c50
Trey Coggins <treycoggins@surfturkey.net>
2022-05-04 23:44:21 -0400

    add blog and small logo

--- scripts/app.js
+++ scripts/app.js
@@ -17,3 +17,13 @@ window.onclick = function(event) {
     contactForm.style.display = "none";
   }
 }
+
+
+// Show/hide Dropdown menu on navbar
+
+ const dropdown = document.querySelector('#dropdownBtn');
+ const dropMenu = document.querySelector('.dropdown__list');
+
+ dropdown.addEventListener('click', () => {
+   dropMenu.style.display = 'block';
+ });
