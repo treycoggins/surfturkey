2086bd1473b0dc0bc8a3183e1e7fb922accbd5e6
Trey Coggins <coggins.mp3@gmail.com>
2022-05-12 18:09:55 -0400

    update index.js to add port variable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, '/public')));
@@ -16,7 +17,7 @@ app.get('/blog', (req, res) => {
   res.render('blog');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('LISTENING ON PORT 3000');
 });
 
